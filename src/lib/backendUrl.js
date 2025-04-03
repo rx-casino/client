c97a864ebea2e8d3806826cf174091b57be54580
Highscoretech <highscoretechng@gmail.com>
2025-04-03 15:57:15 +0100

    keys

--- src/lib/backendUrl.js
+++ src/lib/backendUrl.js
@@ -3,7 +3,7 @@ import { browser } from '$app/environment'
 export const serverUrl = () => {
   if(browser){
     let url = location.hostname === "localhost" || location.hostname === "127.0.0.1" 
-    ? "http://localhost:8000" : "https://cat3poker-d07112c3d84c.herokuapp.com"
+    ? "http://localhost:8000" : "https://rx-casino.onrender.com"
      return url
   }
 }
@@ -13,7 +13,7 @@ export const clientUrl = () => {
     if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
       return "http://localhost:5173";
     }else{
-      return "https://playtrickygames.netlify.app";
+      return "https://rx-casino.netlify.app";
     }
   }
 }
