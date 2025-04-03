33f8f55207642640bb32155f725996f10b2ada30
Highscoretech <highscoretechng@gmail.com>
2025-04-03 15:11:45 +0100

    free

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,6 @@
     // import { app } from "$lib/store/screen.js";
 </script>
 
-
 <div class="sc-eCImPb cuPxwd empty full-abs">
     <img alt="" src="/assets/logo.png">
     <div class="msg">Page you're looking for is not created! </div>
